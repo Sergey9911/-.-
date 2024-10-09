1db735213194f054db3a3045acd7b516f66941ba
Sergey9911 <kormilin.sergey@gmail.com>
2024-10-09 15:59:27 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,8 @@
+students={'Jonny': [5,3,3,5,4], 'Bilbo': [2,2,2,3], 'Steve': [4,5,5,2], 'Khendrik': [4,4,3], 'Aaron': [5,5,5,4,4]}
+print(students)
+students['Jonny'] = float(5+3+3+5+4) / 5
+students['Bilbo'] = float(2+2+2+3) / 4
+students['Steve'] = float(4+5+5+2) / 4
+students['Khendrik'] = float(4+4+3) / 3
+students['Aaron'] = float(5+5+5+4+4) /5
+print(students)
